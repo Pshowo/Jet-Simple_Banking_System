1f83d66bdbbdaac2e9f276a9ac6865f3d962f54c
Pshowo <pjuszcze@gmail.com>
2020-07-18 18:37:44 +0200

    Stage 4/4: Advanced system

--- Simple_Banking_System.py
+++ Simple_Banking_System.py
@@ -3,8 +3,9 @@ import random
 import sqlite3
 import os
 
-
 credit_cards = {}
+
+
 def create_table(cur):
     cur.execute(""""
         CREATE TABLE IF NOT EXISTS card (
@@ -15,8 +16,10 @@ def create_table(cur):
         );""")
     cur.commit()
 
+
 # print menu
 def menu():
+    print("")
     print("1. Create an account")
     print("2. Log into account")
     print("0. exit")
@@ -25,11 +28,16 @@ def menu():
 
 
 def menu_account():
+    print("")
     print("1. Balance")
-    print("2. Log out")
+    print("2. Add income")
+    print("3. Do transfer")
+    print("4. Close account")
+    print("5. Log out")
     print("0. Exit")
     choose = input()
     return choose
+
 
 def luhn(num):
     # multiply odd digits by 2
@@ -52,6 +60,7 @@ def luhn(num):
         sum_number += char
     check_sum = int(round((math.ceil(sum_number / 10) - sum_number / 10), 1) * 10)
     return check_sum
+
 
 def create_account():
     print("Your card has been created")
@@ -88,29 +97,85 @@ def add_card_to_db(card_num, PIN):
     cur.execute(sql_add_card)
     conn.commit()
     id_card += 1
-    print("Add card to db")
 
 
-
-
-def log_into():
+def log_into(conn):
     global credit_cards
     print("Enter your card number:")
     card_num = str(input())
 
-    print("Enter your PIN::")
-    PIN = int(input())
+    cur = conn.cursor()
+    cur.execute("SELECT number,PIN FROM card WHERE number='{}'".format(card_num))
+    card_in_base = cur.fetchall()
 
-    if card_num in credit_cards:
-        if credit_cards[card_num]["PIN"] == PIN:
+    if len(card_in_base) == 1:
+        print("Enter your PIN::")
+        PIN = input()
+        if PIN == card_in_base[0][1]:
             print("You have successfully logged in!")
+
+            # In account
             while True:
                 choose = menu_account()
-                if choose == "1":
-                    print("Balance: {}".format(credit_cards[card_num]["Balance"]))
-                elif choose == "2":
-                    print("You have successfully logged out!")
+                if choose == "1":  # balance
+                    cur.execute("SELECT Balance FROM card WHERE number='{}'".format(card_num))
+                    balance = cur.fetchall()
+                    print("Balance: {}".format(balance[0][0]))
+
+                    continue
+
+                elif choose == "2":  # add income
+                    print("Enter income:")
+                    income = input()
+                    cur.execute("UPDATE card SET balance=balance+{} WHERE number={}".format(income, card_num))
+                    conn.commit()
+                    print("Income was added!")
+                    continue
+
+                elif choose == '3':  # do transfer
+                    print("Enter card number:")
+                    other_card = input()
+                    if other_card == card_num:
+                        print("You can't transfer money to the same account!")
+                    else:
+
+
+                        if str(luhn(other_card[:-1])) == str(other_card[-1:]):
+                            cur.execute("SELECT number FROM card WHERE number='{}'".format(other_card))
+                            other_card = cur.fetchall()
+                            cur.execute("SELECT balance FROM card WHERE number='{}'".format(card_num))
+                            my_balance = cur.fetchall()[0][0]
+
+                            if len(other_card) == 0:
+                                print("Such a card does not exist.")
+                            else:
+                                print("Enter how much money you want to transfer:")
+                                money = input()
+
+
+                                if int(money) > int(my_balance):
+                                    print('Not enough money!')
+                                    continue
+                                else:
+                                    print(money, other_card[0][0])
+                                    cur.execute(
+                                        "UPDATE card SET balance=balance-{} WHERE number={}".format(money, card_num))
+                                    cur.execute("UPDATE card SET balance=balance+{} WHERE number={}".format(money,
+                                                                                                            other_card[0][
+                                                                                                                0]))
+                                    conn.commit()
+                                    print("Success!")
+                                    continue
+                        else:
+                            print("Probably you made mistake in the card number. Please try again!")
+                elif choose == '4': # delete account
+                    cur.execute("DELETE FROM card WHERE number={}".format(card_num))
+                    conn.commit()
                     break
+
+                elif choose == '5':
+                    break
+
                 elif choose == "0":
                     print("Bye!")
                     exit()
@@ -118,6 +183,7 @@ def log_into():
             print("Wrong card number or PIN!")
     else:
         print("Wrong card number or PIN!")
+
 
 # ============
 
@@ -135,14 +201,14 @@ sql_create_table = """
 cur.execute(sql_create_table)
 conn.commit()
 
-
 while True:
     choose = menu()
     if choose == "1":
         create_account()
     elif choose == "2":
-        log_into()
+        log_into(conn)
     elif choose == "0":
         print("Bye!")
         break
-
+conn.commit()
+conn.close()
